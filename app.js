af9d05bee67ada1244bc6d590136c781d2fb2eea
Vatsal32 <vatsalm707@gmail.com>
2023-01-29 00:44:45 +0530

    NFT minting backend

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const UserRoute = require("./src/routes/UserRoutes");
+const ProductRoute = require("./src/routes/ProductRoutes");
+const NFTRoute = require("./src/routes/NFTRoutes");
 require("dotenv").config();
 const cors = require("cors");
 
@@ -9,6 +11,8 @@ const corsOption = {
   origin: ["http://localhost:3000"],
 };
 app.use(cors(corsOption));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 const port = process.env.PORT;
 
@@ -31,6 +35,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users/", UserRoute);
+
+app.use("/api/products/", ProductRoute);
+
+app.use("/api/nft/", NFTRoute);
 
 app.listen(port, () => {
   console.log(`🚀 Server running on port: ${port}`);
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,93 @@
+const UserModel = require("../models/Users");
+const CompanyModel = require("../models/Company");
+const ProductModel = require("../models/Products");
+const ObjectId = require("mongoose").Types.ObjectId;
+const cloudinary = require("cloudinary").v2;
+const Data_uri = require("datauri/parser");
+const path = require("path");
+
+require("dotenv").config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+module.exports = {
+  addProduct: async (req, res) => {
+    const category_name = req.body.category_name || "";
+    const user_id = req.body.user_id || "";
+
+    if (category_name === "") {
+      res.json({
+        error: "Invalid Inputs",
+        data: {
+          category_name: "Enter a valid category name",
+        },
+      });
+    }
+
+    if (user_id === "") {
+      res.json({
+        error: "Invalid Inputs",
+        data: {
+          user_id: "Enter a valid User ID",
+        },
+      });
+    }
+
+    if (req.file === undefined || req.file === null) {
+      res.json({
+        error: "Invalid Inputs",
+        data: {
+          image: "Enter a valid image",
+        },
+      });
+    } else {
+      let data_uri_parser = new Data_uri();
+      data_uri_parser.format(
+        path.extname(req.file.originalname).toString(),
+        req.file.buffer
+      );
+      const file = data_uri_parser.content;
+
+      const newProduct = new ProductModel({
+        category_name,
+        user_id,
+        img: "temp",
+      });
+
+      await cloudinary.uploader.upload(
+        file,
+        {
+          resource_type: "image",
+          public_id: `images/${newProduct.category_name}`,
+          overwrite: true,
+        },
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            newProduct.img = result.secure_url;
+            newProduct
+              .save()
+              .then(() => {
+                res.json({
+                  message: "Success",
+                  data: {},
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.json({
+                  error: "Something went wrong",
+                  data: {},
+                });
+              });
+          }
+        }
+      );
+    }
+  },
+};
