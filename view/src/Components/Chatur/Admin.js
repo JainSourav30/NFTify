56abedb120e187b99949da9d0f8add8c4c075c41
Pranav Chatur <pranavchatur@gmail.com>
2023-01-29 09:03:49 +0530

    Add Admin Page

--- view/src/Components/Chatur/Admin.js
+++ view/src/Components/Chatur/Admin.js
@@ -3,13 +3,41 @@ import ConnRequest from "./ConnRequest";
 
 const Admin = () => {
 
-	return (
-    	<>
-            <ConnRequest name={"Pranav"} phone={12343} mail={"123@g.com"} address={"0x133hfee7"}/>
-            <ConnRequest name={"Pranav"} phone={12343} mail={"123@g.com"} address={"0x133hfee7"}/>
-            <ConnRequest name={"Pranav"} phone={12343} mail={"123@g.com"} address={"0x133hfee7"}/>
+    const companies = [
+        {
+            name:'Pranav',
+            phone:1234,
+            mail:'123@g.com',
+            address: '0x133hfee7'
+        },
+        {
+            name:'Raghav',
+            phone:23445,
+            mail:'123@g.com',
+            address: '0x133hfee7'
+        },
+        {
+            name:'Sourav',
+            phone:33455,
+            mail:'123@g.com',
+            address: '0x133hfee7'
+        },
+        {
+            name:'Vatsal',
+            phone:7899,
+            mail:'123@g.com',
+            address: '0x133hfee7'
+        },
+    ]
 
-        </>
+	return (
+    	<div className="flex flex-col justify-around items-center bg-nft bg-contain h-screen w-full">
+            <div className="text-2xl font-bold text-white">Administrator Panel</div>
+            {companies.map((company)=>{
+                return <ConnRequest name={company.name} phone={company.phone} mail={company.phone} address={company.address}/>
+            })}
+
+        </div>
     );
 }
 
