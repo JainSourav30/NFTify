add1288262a2ee20c393d0b50ebb3e29a1c73216
Raghav Khanna <traxincoder@pop-os.localdomain>
2023-01-29 10:12:28 +0530

    Add Avatar in Home Page

--- view/src/Components/Chatur/Avatar.js
+++ view/src/Components/Chatur/Avatar.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
-import {RxAvatar} from "react-icons/rx";
+import { RxAvatar } from "react-icons/rx";
 import ButtonGroup from "./ButtonGroup";
 
-const Avatar = ({size}) => {
-    const [showMenu, setShowMenu] = useState(0);
+const Avatar = ({ size }) => {
+  const [showMenu, setShowMenu] = useState(0);
 
-	return (
-    	<div className="flex flex-col justify-center overflow-visible">
-            <button onClick={(e)=>{e.preventDefault();setShowMenu(!showMenu);}}>
-                <RxAvatar size={size} className="m-auto p-0"/>
-            </button>
-            {showMenu?<ButtonGroup/>: null}
+  return (
+    <div className="flex flex-col justify-center overflow-visible">
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setShowMenu(!showMenu);
+        }}
+      >
+        <RxAvatar size={size} className="m-auto p-0" />
+      </button>
+      {showMenu ? (
+        <div className="absolute overflow-auto top-12 right-14">
+          <ButtonGroup />
         </div>
-    );
-}
+      ) : null}
+    </div>
+  );
+};
 
 export default Avatar;
